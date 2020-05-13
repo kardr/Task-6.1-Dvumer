bcc116cf2e3939d3eb128e7f5816f7785d225864
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-13 16:09:12 +0300

    .

--- Task 6.1 Dvumer/Program.cs
+++ Task 6.1 Dvumer/Program.cs
@@ -10,6 +10,39 @@ namespace Task_6._1_Dvumer
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Введите размерность массива:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int m = Convert.ToInt32(Console.ReadLine());
+                int[,] mass = new int[n, m];
+                Random r = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        mass[i, j] = r.Next(-50, 50);
+                        Console.Write(mass[i, j] + "   ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                for (int i = 1; i < n; i = i + 2)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (mass[i, j] > 0)
+                        {
+                            Console.Write(mass[i, j] + "   ");
+                        }
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
